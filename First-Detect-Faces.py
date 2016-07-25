15b88036d7941f9d10b83355638d0ba7c174a7ec
Ryan O'Leary <ryan.oleary@jila.colorado.edu>
2016-07-25 01:11:58 -0600

    face and eye detection for individual images

--- First-Detect-Faces.py
+++ First-Detect-Faces.py
@@ -6,12 +6,12 @@
 # We are going to follow the opencv documentation to find the eyes in the image:
 # http://docs.opencv.org/master/d7/d8b/tutorial_py_face_detection.html#gsc.tab=0
 
-# In[2]:
+# In[1]:
 
 get_ipython().magic(u'pylab inline')
 
 
-# In[10]:
+# In[2]:
 
 import cv2
 import numpy as np
@@ -20,30 +20,30 @@ import time
 
 # XML classifiers downloaded from opencv.org
 
-# In[4]:
+# In[135]:
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 
-# In[81]:
+# In[136]:
 
 eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
 
 # Import the image and load grayscale version of it
 
-# In[117]:
+# In[159]:
 
-img = cv2.imread('imgs/20160306_IMG_2791p.JPG')
+img = cv2.imread('imgs/20160306_IMG_2792p.JPG')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgmp =  cv2.cvtColor(img, cv2.cv.CV_BGR2RGB) #this is to plot images using matplotlib
 
 
-# In[118]:
+# In[160]:
 
 t1 = time.time()
 #detect faces
-faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+faces = face_cascade.detectMultiScale(gray, 1.4, 6, minSize=(40,40))
 #this could be sped up by rescaling large images
 t2 = time.time()
 print "It took ", t2-t1, "seconds to complete faces."
@@ -56,13 +56,18 @@ print "It took ", t3-t2, "seconds to complete eyes.\n\n"
 print "Found ", len(faces), " faces and ", len(eyes), " eyes."
 
 
-# In[119]:
+# In[161]:
+
+faces
+
+
+# In[162]:
 
 #find largest face
 index = (faces[:,3]*faces[:,2]).argmax()
 
 
-# In[120]:
+# In[164]:
 
 imshow(imgmp)
 x,y,w,h = faces[index]
@@ -73,18 +78,43 @@ for (ex,ey,ew,eh) in eyes:
 axis([x-2.5*w,x+3.5*w,y+3.5*h,y-2.5*h])
 
 
-# In[121]:
+# In[165]:
 
-#plot all faces
-imshow(imgmp)
-for (x,y,w,h) in faces:
-    fill_between([x,x+w],[y,y],[y+h,y+h],color='None',edgecolor='w')
+#percentile?
 
 
-# In[90]:
+# In[166]:
 
-roi_gray.shape
+#calculate contrast inside face region
+#We use a Laplacian filter to estimate the amount of contrast in the image following http://www.pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
+x,y,w,h = faces[index]
+faceimg = gray[y:y+h, x:x+w]
+lap_faceimg = cv2.Laplacian(faceimg, 8)
+eyestd = []
+per90 = []
+per99 = []
+eyeimgs = []
+for (ex,ey,ew,eh) in eyes:
+    eyeimg = faceimg[ey:ey+eh, ex:ex+ew]
+    eyeimgs.append(eyeimg)
+    lap_eyeimg = cv2.Laplacian(eyeimg, 8)
+    eyestd.append( lap_eyeimg.std())
+    per99.append(percentile(lap_eyeimg,99.9))
+print "Face std", lap_faceimg.std(), " and eye stds ", eyestd, per99, percentile(lap_faceimg,99.9)
 
+
+# In[167]:
+
+if( lap_faceimg.std() > max(eyestd) ):
+    print "Face is Most in focus"
+else:
+    print "Eye #", argmax(eyestd) + 1, " is most in focus"
+    imshow(eyeimgs[argmax(eyestd)],cmap='gray')
+
+
+# ### Todo:
+# 
+# Iteratively update cascade parameters to only get two eyes and ensure at least one face
 
 # In[ ]:
 
